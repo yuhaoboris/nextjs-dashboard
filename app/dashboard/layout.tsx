81beb742d6d156ec2a6181f5664dbfdf66a02113
boris <yuhaoboris@gmail.com>
2024-05-07 15:22:08 +0800

    fix type error

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,10 @@
 import SideNav from '../ui/dashboard/sidenav';
 
-export default function DashboardLayout({ children }) {
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
